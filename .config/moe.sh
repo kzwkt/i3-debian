0ca134d53594ad5074b6ccc786f929c9a319b4ee
kzwkt <9437935+kzwkt@users.noreply.github.com>
2019-03-07 19:26:33 +0000

    Add files via upload

---
+++ .config/moe.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+declare -a arr=('sudo ntfs-3g /dev/sda3 /home/kai/mnt/win''sudo mount /dev/sda4 /home/kai/mnt/pho''sudo ntfs-3g /dev/sda5 /home/kai/mnt/aps' 'sudo umount /home/kai/mnt/win' 'sudo umount /home/kai/mnt/aps' 'sudo umount /home/kai/mnt/pho')
+
+$usbdrives="apple"
+echo "$usbdrives" | dmenu -i -p "Mount which drive?" | awk '{print $1}'	
+
+if [ $# -eq 0 ]; then 
+echo "win[0/3],pho[1/4],aps[2/4]]"
+fi
+while [ $# -gt 0 ]
+do
+exec ${arr[$1]}
+done
+
